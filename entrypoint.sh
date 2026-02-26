323776577a10b2f0aa9fef3f1e50dedf2334ab03
manuroc <manuroc@users.noreply.github.com>
2026-02-26 22:36:20 +0900

    Change volume check from ls to db.php existence

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Check if volume is empty
-if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
+if [ ! -f "/var/www/wp-content/db.php" ]; then
     echo 'Setting up wp-content volume'
     # Copy wp-content from Wordpress src to volume
     cp -r /usr/src/wordpress/wp-content /var/www/
